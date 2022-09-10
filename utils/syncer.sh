ba5be8cb19de81abc4dc3ef756a5343e818a4615
semperubique <lastmurname@gmail.com>
2022-09-10 02:29:34 +0200

    Add a case where file exists only in 1 directory

--- utils/syncer.sh
+++ utils/syncer.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-# todo
-# 3 cases:
-# 	1) file exists in both directories
-# 	2) file only exists in d1
-# 	3) file only exists in d2
-
-#for file_d1 in ${d1}*; do # for each file in d1
-#	echo "-Checking for $(basename $file_d1) in d1"
-#	for file_d2 in ${d2}*; do # for each file in d2
-#		echo "--Checking for $(basename $file_d2) in d2"
-#		if [ $(basename $file_d1) == $(basename $file_d2) ]; then # if the filenames are the same 
-#			if [ $(date -r $file_d1 "+%s") -gt $(date -r $file_d2 "+%s") ]; then # compare timestamps
-#				$(rsync -t $file_d1 $file_d2); # file in d1 survives
-#				echo "---Synced $(basename $file_d1) from d1 to d2";
-#			elif [ $(date -r $file_d1 "+%s") -lt $(date -r $file_d2 "+%s") ]; then
-#				$(rsync -t $file_d2 $file_d1); # file in d2 survives
-#				echo "---Synced $(basename $file_d2) from d2 to d1";
-#			else
-#				echo "---Files are in sync";
-#			fi
-#		else
-#			echo "--$(basename $file_d1) exists only in d1";
- #       	fi
-#	done
-#done
-
 function sync_connections {
 	for connection in ./connections/*; do
 		echo "Syncing connection $(basename -s .cfg $connection):";
@@ -46,5 +20,8 @@ function sync_connections {
 		done
 		
 		# 2nd case: when file exists only in one directory
+		for file in $(diff -rs $d1 $d2 | grep "Only" | awk '{print $4}'); do
+			echo "$file ony in $(diff -rs $d1 $d2 | grep "Only" | grep "$file" | awk '{print $3}')";
+		done
 	done
 }
