c7129bfc4ebdb4478f3a9f806653553809d9b707
semperubique <lastmurname@gmail.com>
2022-09-11 07:39:53 +0200

    Cover second case

--- utils/connector.sh
+++ utils/connector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function list_connections {
+function list_connections() {
 	echo "Listing all connections:";
 	echo $(ls ./connections/ | cut -d "." -f 1);
 }
@@ -18,4 +18,20 @@ function remove_connection {
 	echo "Removing a connection";
 	echo -e "Enter the name of connection you want to remove: \c"; read connection_name;
 	$(rm -f ./connections/${connection_name}".cfg");
+}
+
+function sync_time_of_connection {
+	last_sync_time=$(awk -F "=" '/last_sync_time/ {print $2}' $1);
+	d1=$(awk -F "=" '/d1/ {print $2}' $1);
+	d2=$(awk -F "=" '/d2/ {print $2}' $1);
+
+	if [ -z $last_sync_time ]; then
+		echo "The connection is new, initializing the last-sync time to the oldest file in both directories";
+		oldest_file=$(find $d1 $d2 -type f -printf '%T+ %p\n' | sort | head -1 | awk '{print $2}');
+		oldest_file_time=$(TZ='UTC' date -r $oldest_file "+%s");
+		echo "Oldest file: $oldest_file";
+		echo "last_sync_time=${oldest_file_time}" >> $1;
+	else
+		echo "Last synced: $(TZ='UTC' date -d @$last_sync_time)";
+	fi
 }
--- utils/syncer.sh
+++ utils/syncer.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+source ./utils/connector.sh
+
 function sync_connections {
 	for connection in ./connections/*; do
 		echo "Syncing connection $(basename -s .cfg $connection):";
+		sync_time_of_connection $connection;
+		last_sync_time=$(awk -F "=" '/last_sync_time/ {print $2}' $connection);
 		d1=$(awk -F "=" '/d1/ {print $2}' ${connection});
 		d2=$(awk -F "=" '/d2/ {print $2}' ${connection});
 		
 		# 1st case: when file exists in both directories
 		for file in $(find $d1 $d2 -printf '%P\n' | sort | uniq -d); do
-			if [ $(date -r ${d1}${file} "+%s") -gt $(date -r ${d2}${file} "+%s") ]; then
+			if [ $(TZ='UTC' date -r ${d1}${file} "+%s") -gt $(TZ='UTC' date -r ${d2}${file} "+%s") ]; then
 				echo "$file: d1 version is newer, syncing from d1 to d2";
 				$(rsync -t ${d1}${file} ${d2}${file});
-			elif [ $(date -r ${d1}${file} "+%s") -lt $(date -r ${d2}${file} "+%s") ]; then
+			elif [ $(TZ='UTC' date -r ${d1}${file} "+%s") -lt $(TZ='UTC' date -r ${d2}${file} "+%s") ]; then
 				echo "$file: d2 version is newer, syncing from d2 to d1";
 				$(rsync -t ${d2}${file} ${d1}${file});
 			else
@@ -21,7 +25,25 @@ function sync_connections {
 		
 		# 2nd case: when file exists only in one directory
 		for file in $(diff -rs $d1 $d2 | grep "Only" | awk '{print $4}'); do
-			echo "$file ony in $(diff -rs $d1 $d2 | grep "Only" | grep "$file" | awk '{print $3}')";
+			file_directory=$(diff -rs $d1 $d2 | grep "Only" | grep "$file" | awk '{print substr($3, 1, length($3)-1)}');
+			if [ $file_directory == $d1 ]; then
+				other_directory=$d2;
+			else
+				other_directory=$d1;
+			fi
+			echo "$file exists only in $file_directory";
+			if [ $(TZ='UTC' date -r ${file_directory}${file} "+%s") -ge $last_sync_time ]; then
+				echo "File is newly added to $file_directory, adding it to $other_directory too";
+				$(date -p ${file_directory}{$file} ${file_direcotry}{$file});
+			else
+				echo "File is deleted in $other_directory, deleting it in $file_directory too";
+				$(rm -rf ${file_directory}${file});
+			fi
+			
 		done
+		
+		time_now=$(TZ='UTC' date "+%s")
+		$(sed -i "s/last_sync_time=.*/last_sync_time=${time_now}/" $connection);
+		echo "Last-sync time is updated to $(TZ='UTC' date)";
 	done
 }
